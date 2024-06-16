66bdf95b582585769902e681447a6dbbad76b380
akhdrzki <akhadirizky@gmail.com>
2024-06-16 21:50:40 +0800

    feat: init interface test

---
+++ test/interface.test.ts
@@ -0,0 +1,25 @@
+import { Employee } from "../src/employee";
+import { Person } from "../src/person";
+
+describe("Interface", function () {
+  it("should support extends interface", function () {
+    const employee: Employee = {
+      id: "1",
+      name: "Rizki",
+      division: "IT",
+    };
+
+    console.info(employee);
+  });
+
+  it("should support function in interface", function () {
+    const person: Person = {
+      name: "Rizki",
+      sayHello: function (name: string): string {
+        return `Hello ${name}, my name is ${this.name}`;
+      },
+    };
+
+    console.info(person.sayHello("Akhadi"));
+  });
+});
