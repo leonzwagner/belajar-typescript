d2764ce6d5e5fb16bf1ed7c1060cd6ffbf003c10
akhdrzki <akhadirizky@gmail.com>
2024-06-17 02:45:40 +0800

    feat: init tipe data test

---
+++ test/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("Data type", function () {
+  it("must be declare", function () {
+    const name: string = "Lalu Akhadi Rizki";
+    const balance: number = 10000;
+    const isVip: boolean = false;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+  });
+});
