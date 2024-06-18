93f3a7e727e0ec3d9fe2dab06094d90d32da9ca5
akhdrzki <akhadirizky@gmail.com>
2024-06-18 09:41:11 +0800

    chore: add extends interface, function interface

--- test/interface.test.ts
+++ test/interface.test.ts
@@ -1,4 +1,4 @@
-import { Employee } from "../src/employee";
+import { Employee, Manager } from "../src/employee";
 import { Person } from "../src/person";
 import { Seller } from "../src/seller";
 
@@ -11,6 +11,15 @@ describe("Interface", function () {
     };
 
     console.info(employee);
+
+    const manager: Manager = {
+      id: "2",
+      name: "Akhadi",
+      division: "IT",
+      numberOfEmployees: 15,
+    };
+
+    console.info(manager);
   });
 
   it("should support function in interface", function (): void {
@@ -29,8 +38,45 @@ describe("Interface", function () {
       id: 1,
       name: "Toko Rizk",
       address: "Jl. Gaharu",
+      nib: "11223344",
+      npwp: "11223344",
     };
 
     console.info(seller);
+  });
+
+  it("should support func interface", function () {
+    interface AddFunction {
+      (value1: number, value2: number): number;
+    }
+
+    const add: AddFunction = (value1: number, value2: number): number => {
+      return value1 + value2;
+    };
+
+    expect(add(2, 4)).toBe(6);
+  });
+
+  it("should support indexable interface", function () {
+    interface StringArray {
+      [index: number]: string;
+    }
+
+    const names: StringArray = ["Akhadi", "Lalu", "Rizki"];
+    console.info(names);
+  });
+
+  it("should support indexable interface for non number index", function () {
+    interface StringDict {
+      [key: string]: string;
+    }
+
+    const dictionary: StringDict = {
+      name: "Akhadi",
+      address: "Indonesia",
+    };
+
+    expect(dictionary["name"]).toBe("Akhadi");
+    expect(dictionary["address"]).toBe("Indonesia");
   });
 });
